9649ec56e22a92161744726f199b50ba061c7ebb
KeshavGithub1998 <keshavdamanikjj@gmail.com>
2019-10-25 22:55:21 +0530

    music lib hanges

--- app/src/main/java/com/example/vivek/music/testing.java
+++ app/src/main/java/com/example/vivek/music/testing.java
@@ -11,7 +11,7 @@ class Pair{
 	// according the first (or smaller) values in pairs. 
 	static int maxChainLength(Pair arr[], int n) 
 	{ 
-	int i, j, max = 0; 
+	int i, j, mi = 0; 
 	int mcl[] = new int[n]; 
 	
 	/* Initialize MCL (max chain length) values for all indexes */
@@ -28,8 +28,28 @@ class Pair{
 	
 	/* Pick maximum of all MCL values */
 	for ( i = 0; i < n; i++ ) 
-		if ( max < mcl[i] ) 
-			max = mcl[i]; 
+		if ( mi >=mcl[i] ) 
+			mi = mcl[i]; 
+	
+	return mi; 
+	} 
+
+	int minL(Pair arr[], int n) 
+	{ 
+	int i, j, max = 0; 
+	int mcl[] = new int[n]; 
+	
+	/* Initialize MCL (max chain length) values for all indexes */
+	for ( i = 0; i < n; i++ ) 
+		mcl[i] = 1; 
+	
+	/* Compute optimized chain length values in bottom up manner */
+	for ( i = 1; i < n; i++ ) 
+		for ( j = 0; j < i; j++ ) 
+			if ( arr[i].a > arr[j].b && mcl[i] < mcl[j] + 1) 
+				mcl[i] = mcl[j] + 1; 
+	
+	
 	
 	return max; 
 	} 
